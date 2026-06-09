3d70c333bbf1d82ebb11c7c96c5e6ab2fc175272
kamaldeepk4003 <kamaldeep.kaur@msfa.christuniversity.in>
2026-06-09 16:10:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pandas as pd
 # =====================================================
 
 st.set_page_config(
-    page_title="Training Resource Dashboard",
+    page_title="Training Calendar",
     page_icon="📚",
     layout="wide"
 )
