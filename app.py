3e47b62a2a652e54731af0e241a21a53715f12e2
kamaldeepk4003 <kamaldeep.kaur@msfa.christuniversity.in>
2026-06-15 16:29:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -304,66 +304,6 @@ st.plotly_chart(fig3, use_container_width=True)
 # CALENDAR EVENTS
 # =====================================================
 
-from datetime import timedelta
-
-events = []
-
-for _, row in filtered_df.iterrows():
-
-    start_date = pd.to_datetime(row["Start date"])
-    end_date = pd.to_datetime(row["Closing date"])
-
-    class_day = str(row["CLASS DAYS"]).upper()
-    class_time = str(row["CLASS TIME"])
-
-    # ONLINE = GREEN
-    if str(row["Delivery mode"]).upper() == "ONLINE":
-
-        bg_color = "#22C55E"
-        border_color = "#16A34A"
-
-    # OFFLINE = BLUE
-    else:
-
-        bg_color = "#3B82F6"
-        border_color = "#2563EB"
-
-    day_map = {
-        "MONDAY": 0,
-        "TUESDAY": 1,
-        "WEDNESDAY": 2,
-        "THURSDAY": 3,
-        "FRIDAY": 4,
-        "SATURDAY": 5,
-        "SUNDAY": 6
-    }
-
-    target_day = day_map.get(class_day)
-
-    current = start_date
-
-    while current <= end_date:
-
-        if current.weekday() == target_day:
-
-            start_dt = pd.to_datetime(
-                f"{current.date()} {class_time}"
-            )
-
-            end_dt = start_dt + timedelta(hours=2)
-
-            events.append({
-                "title": f"{row['Program']} | {row['Mapped Trainers']}",
-                "start": start_dt.isoformat(),
-                "end": end_dt.isoformat(),
-
-                "backgroundColor": bg_color,
-                "borderColor": border_color,
-                "textColor": "white"
-            })
-
-        current += timedelta(days=1)
-
 #========= Online/offline marker
 
 st.markdown("""
